c0d2cbc91e11da98ce1b22c6851c2a7efbe01811
younusshamim <younusshamim.bd@gmail.com>
2023-08-09 02:34:13 +0600

    server api connect

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://electro-recyclr-server.vercel.app/",
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://electro-recyclr-server.vercel.app/",
+  // baseURL: "http://localhost:5000/",
   //   timeout: 10000,
 });
 
